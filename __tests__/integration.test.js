4cc0cb9dcbdf835888947c90bf5131f35d028b20
Lewis Price <1995ldp@gmail.com>
2023-04-17 17:43:43 +0100

    completes getCategories

---
+++ __tests__/integration.test.js
@@ -0,0 +1,18 @@
+const request = require("supertest");
+const seedDB = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const db = require("../db/connection");
+const app = require("../app");
+
+beforeAll(() => seedDB(data));
+afterAll(() => db.end());
+
+describe("/api/categories", () => {
+  describe("GET", () => {
+    test("200 - Returns an array of category objects on the key of categories", async () => {
+      const { status, body } = await request(app).get("/api/categories");
+      expect(status).toBe(200);
+      expect(body.categories.length).toBe(4);
+    });
+  });
+});
